3d7a72b4f42524dff169694fb8249c94d96678e6
nefrob <nefrob.skyguy@gmail.com>
2021-10-02 13:37:44 -0600

    Setup hardhat and simple wager token with test

---
+++ scripts/deploy-script.js
@@ -0,0 +1,31 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+    // Hardhat always runs the compile task when running scripts with its command
+    // line interface.
+    //
+    // If this script is run directly using `node` you may want to call compile
+    // manually to make sure everything is compiled
+    // await hre.run('compile');
+
+    // TODO: make available in non-testing environment
+
+    const WagerToken = await hre.ethers.getContractFactory("WagerToken");
+    const wagerTokenInstance = await WagerToken.deploy();
+    await wagerTokenInstance.deployed();
+    console.log("Wager token deployed address:", wagerTokenInstance.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
---
+++ test/token-test.js
@@ -0,0 +1,31 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Wager token tests", function () {
+    it("Mint new tokens", async function () {
+        const accounts = await ethers.getSigners();
+
+        const WagerToken = await hre.ethers.getContractFactory("WagerToken");
+        const token = await WagerToken.deploy();
+        await token.deployed();
+
+        let supply = await token.totalSupply();
+        expect(supply).to.equal(0);
+
+        await token.connect(accounts[0]).mint(10);
+        let balance1 = await token.balanceOf(accounts[0].address);
+        supply = await token.totalSupply();
+
+        expect(balance1).to.equal(10);
+        expect(supply).to.equal(10);
+
+        await token.connect(accounts[1]).mint(20);
+        balance1 = await token.balanceOf(accounts[0].address);
+        let balance2 = await token.balanceOf(accounts[1].address);
+        supply = await token.totalSupply();
+
+        expect(balance1).to.equal(10);
+        expect(balance2).to.equal(20);
+        expect(supply).to.equal(30);
+    });
+});
